aee7e076d002ccc66afc2cfa7aa07e55450df186
Sean McCarty <soxfansean@gmail.com>
2024-11-27 15:06:54 -0500

    Add script to run docker container

---
+++ game_cart/run_docker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+IMAGE_NAME="game_cart"
+CONTAINER_TAG="0.0.1"
+HOST_PORT=5000
+CONTAINER_PORT=5000
+DB_VOLUME_PATH="./db"
+BUILD=true
+
+if ["$BUILD" = true ]; then
+    echo "Building Docker image..."
+    docker build -t ${IMAGE_NAME}:${CONTAINER_TAG} .
+else 
+    echo "Skipping Docker image build..."
+fi
+
+if [ ! -d "${DB_VOLUME_PATH}" ]; then
+    echo "Creating database directory at ${DB_VOLUME_PATH}..."
+    mkdir -p ${DB_VOLUME_PATH}
+fi
+
+if [ "$(docker ps -q -a -f name=${IMAGE_NAME}_container)" ]; then
+    echo "Stopping running container: ${IMAGE_NAME}_container"
+    docker stop ${IMAGE_NAME}_container
+
+    if [ $? -eq 0 ]; then
+        echo "Removing container: ${IMAGE_NAME}_container"
+        docker rm ${IMAGE_NAME}_container
+    else
+        echo "Failed to stop container: ${IMAGE_NAME}_container"
+        exit 1
+    fi
+else
+    echo "No running container named ${IMAGE_NAME}_container found."
+fi
+
+echo "Running Docker container..."
+docker run -d \
+  --name ${IMAGE_NAME}_container \
+  --env-file .env \
+  -p ${HOST_PORT}:${CONTAINER_PORT} \
+  -v ${DB_VOLUME_PATH}:/app/db \
+  ${IMAGE_NAME}:${CONTAINER_TAG}
+
+echo "Docker container is running on port ${HOST_PORT}."
