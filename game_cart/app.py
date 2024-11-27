e1b8f3b03997ba3559191c4f860fb505829e1b84
Sean McCarty <soxfansean@gmail.com>
2024-11-27 15:55:14 -0500

    Add db uri for SQLAlchemy in app.py

--- game_cart/app.py
+++ game_cart/app.py
@@ -4,10 +4,13 @@ from game_cart.db import db
 
 app = Flask(__name__)
 
-#db.init_app(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////app/db/app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-#with app.context():
-#    db.create_all()
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/api/health", methods=["GET"])
 def healthcheck() -> Response:
