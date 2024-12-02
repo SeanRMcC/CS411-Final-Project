8bd21e54900467dd293cbfa5011ed7f81abe44b9
Sean McCarty <soxfansean@gmail.com>
2024-12-02 11:07:24 -0500

    Add function to get game details given a game id

--- game_cart/game_cart/utils/cheapsharkapi.py
+++ game_cart/game_cart/utils/cheapsharkapi.py
@@ -12,14 +12,17 @@ base_url = "https://www.cheapshark.com/api/1.0/"
 def search_for_games(keyword: str) -> List[dict[str, Any]]:
     """
         Gets a maximum of ten games and their prices based off of a keyword
-        from cheapshark.com
+        from cheapshark.com.
+
+        Args:
+            keyword (str): The search keyword.       
 
         Returns:
             List[dict[str, Any]]: A list of games where each entry contains
-                the name of the game, the id, and the price
+                the name of the game, the id, and the price.
 
         Raises:
-            RuntimeError: If the request to cheapshark.com times out or causes any other exception
+            RuntimeError: If the request to cheapshark.com times out or causes any other exception.
     """
     url = f"{base_url}games?title={keyword}"
 
@@ -54,5 +57,43 @@ def search_for_games(keyword: str) -> List[dict[str, Any]]:
 
 def get_game_info(gameID: int) -> dict[str, Any]:
     """
-        TODO
+        Gets the price and name of the game corresponding to a given id
+        from cheapshark.com
+
+        Args:
+            gameID (int): The id of the game whose info will be returned.
+
+        Returns:
+            dict[str, Any]: A dict containing the game id, name, and price or an 
+                empty dict if a game does not exist with gameID
+
+        Raises:
+            RuntimeError: If the request to cheapshark.com times out or causes any other exceptions.
     """
+    url = f"{base_url}games?id={gameID}"
+
+    try:
+        logger.info(f"Fetching game info for game with id: {gameID}.")
+        response = requests.get(url, timeout=5)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        return {
+            "name": data["info"]["title"],
+            "id": gameID,
+            "price": data["cheapestPriceEver"]["price"]
+        }
+    except requests.exceptions.HTTPError:
+        logger.warning(f"The id {gameID} does not have a corresponding game.")   
+        return {}
+
+    except requests.exceptions.Timeout:
+        logger.error("Request to cheapshark.com timed out.")
+        raise RuntimeError("Request to cheapshark.com timed out.")
+    
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Request to cheapshark.com failed: {str(e)}")
+        raise RuntimeError(f"Request to cheapshark.com failed: {str(e)}")
+
